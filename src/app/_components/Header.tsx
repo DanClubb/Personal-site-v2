f26d874c09e6e64be6e8014d543a277f2a6770f7
daniel-clubb <danclubb004@gmail.com>
2024-09-19 14:24:18 +0100

    changed font, added cv, made header

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,3 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import styles from "../styles/header.module.css";
+
 export default function Header() {
-    return <header>Header</header>;
+    const [mobileNavExpanded, setMobileNavExpanded] = useState(false);
+    return (
+        <header className={styles.header}>
+            <nav className={styles.nav}>
+                <Link href="/" className={styles.homeNavLink}>
+                    DC
+                </Link>
+                <button
+                    className={styles.mobileNavToggle}
+                    aria-controls="nav"
+                    aria-expanded={mobileNavExpanded}
+                    onClick={() => setMobileNavExpanded((prev) => !prev)}
+                ></button>
+                <ul
+                    className={styles.navLinks}
+                    data-visible={mobileNavExpanded}
+                >
+                    <li>
+                        <Link href="/experience">Experience</Link>
+                    </li>
+                    <li>
+                        <Link href="/projects">Projects</Link>
+                    </li>
+                    <li>
+                        <Link href="/#contact-form">Contact Me</Link>
+                    </li>
+                    <li>
+                        <Link href="/cv.pdf" target="_blank">
+                            Curriculum Vitae
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import Header from "./_components/Header";
 import "./globals.css";
 
-const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Dan Clubb",
@@ -30,7 +21,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>
+            <body className={`${inter.className} `}>
                 <Header />
                 <main>{children}</main>
                 <footer>Footer</footer>
