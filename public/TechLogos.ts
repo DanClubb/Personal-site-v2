58c9c9556968f847f9e02c10fbf3d7502c649c06
daniel-clubb <danclubb004@gmail.com>
2024-09-25 22:38:41 +0100

    added hover effect to socail links in hero

---
+++ public/TechLogos.ts
@@ -0,0 +1,13 @@
+const TECH_LOGOS = {
+    react: require("./tech-icons/reactjs.png").default,
+    // next: require("./tech-icons/next.ico").default,
+    tailwind: require("./tech-icons/tailwind.png").default,
+    javascript: require("./tech-icons/javascript.png").default,
+    typescript: require("./tech-icons/typescript.png").default,
+    html: require("./tech-icons/html.png").default,
+    css: require("./tech-icons/css.png").default,
+    // node: require("./tech-icons/nodejs.png").default,
+    git: require("./tech-icons/git.png").default,
+};
+
+export default TECH_LOGOS;
