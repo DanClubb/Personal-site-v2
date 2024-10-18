ceb6ee54170aa2c0b61d60a3f7301a2fe4bb9308
daniel-clubb <danclubb004@gmail.com>
2024-10-18 23:12:50 +0100

    added drello to projects

--- src/app/data/projectsData.ts
+++ src/app/data/projectsData.ts
@@ -14,19 +14,26 @@ export const projectsData: Project[] = [
         github: "https://github.com/DanClubb/Frontend-Mentor-Challenges",
         image: "/images/project-images/frontend-mentor.webp",
     },
-    {
-        name: "Diamond Gymnastics",
-        description:
-            "I developed an engaging and user-friendly website for diamond gymnastics club, focusing on showcasing class schedules, coach profiles, and club events. This project highlights my ability to create appealing, functional designs tailored to client's needs.",
-        link: "https://www.diamond-gymnastics.com/",
-        github: "https://github.com/DanClubb/diamond",
-        image: "/images/project-images/diamond.png",
-    },
+    // {
+    //     name: "Diamond Gymnastics",
+    //     description:
+    //         "I developed an engaging and user-friendly website for diamond gymnastics club, focusing on showcasing class schedules, coach profiles, and club events. This project highlights my ability to create appealing, functional designs tailored to client's needs.",
+    //     link: "https://www.diamond-gymnastics.com/",
+    //     github: "https://github.com/DanClubb/diamond",
+    //     image: "/images/project-images/diamond.png",
+    // },
     {
         name: "Fragrance Finds",
         description: "",
         link: "https://fragrance-finds-delta.vercel.app/",
         github: "https://github.com/DanClubb/fragrance-finds",
         image: "/images/project-images/frag-finds.png",
+    },
+    {
+        name: "Drello",
+        description: "",
+        link: "https://trello-clone-three-nu.vercel.app/",
+        github: "https://github.com/DanClubb/Trello-Clone",
+        image: "/images/project-images/drello.webp",
     },
 ];
